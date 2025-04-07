3b36d45955c53692c54ea9879b0bc6ed19492ebc
AntoineDecauchy <decauchy.antoine@gmail.com>
2025-04-07 19:24:37 +0200

    ajout de fonction dans node.c pour preparer les fonctions push

--- node.c
+++ node.c
@@ -28,14 +28,57 @@ t_node	*new_node(int index, int value)
 
 void	add_newnode(int index, int value, t_node *first)
 {
+	t_node	*newnode;
+	t_node	*last;
+
 	if (!first)
 		return ;
-	t_node *newnode = new_node(index, value);
+	newnode = new_node(index, value);
 	if (!newnode)
 		return ;
-	t_node *last = first->prev;
+	last = first->prev;
 	last->next = newnode;
 	newnode->prev = last;
 	newnode->next = first;
 	first->prev = newnode;
+}
+
+t_node	*remove_node(t_node **head)
+{
+	t_node	*node;
+
+	if (!head || !*head)
+		return (NULL);
+	node = *head;
+	if (node->next == node)
+		*head = NULL;
+	else
+	{
+		node->prev->next = node->next;
+		node->next->prev = node->prev;
+		*head = node->next;
+	}
+	return (node);
+}
+
+void	insert_node(t_node **head, t_node *node)
+{
+	t_node	*tmp_node;
+
+	tmp_node = NULL;
+	if (!*head)
+	{
+		node->next = node;
+		node->prev = node;
+		*head = node;
+	}
+	else
+	{
+		tmp_node = (*head)->prev;
+		node->next = *head;
+		node->prev = tmp_node;
+		tmp_node->next = node;
+		(*head)->prev = node;
+		*head = node;
+	}
 }
